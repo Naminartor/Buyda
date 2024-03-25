1c638b20df850c6a63576e42ec61bc2a035fa022
wise720 <wisewise720@gmail.com>
2024-03-25 16:49:50 +0100

    some examples how to use express for routing

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const app = express()
 const port = 3000
 app.use(express.static("./public"))
 
+app.get('/hello', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post("/api",(req, res) => {
+    res.send('POST request to the api')
+})
+
+app.get('/users/:userId/', (req, res) => {
+    res.send(req.params)
+})
+app.get('/users/:userId/books/:bookId', (req, res) => {
+    res.send(req.params)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
